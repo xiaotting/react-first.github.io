9a5cd23ccf4e0e1e1b4f0fb45c0a6be3a9cbe32a
zoraXT <451548768@qq.com>
2020-06-27 13:52:10 +0800

    Updates

---
+++ precache-manifest.90c13a9302eb7b97bca41e31e1199451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8c49339284c6d2be73706255532d77",
+    "url": "/xiaotting/react-first.github.io.git/index.html"
+  },
+  {
+    "revision": "a44e2dc54e11a73d6208",
+    "url": "/xiaotting/react-first.github.io.git/static/css/main.026dadde.chunk.css"
+  },
+  {
+    "revision": "2e29891056d3de56ba93",
+    "url": "/xiaotting/react-first.github.io.git/static/js/2.394dcc8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xiaotting/react-first.github.io.git/static/js/2.394dcc8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44e2dc54e11a73d6208",
+    "url": "/xiaotting/react-first.github.io.git/static/js/main.42715313.chunk.js"
+  },
+  {
+    "revision": "8b2474a414dcb5b3bc81",
+    "url": "/xiaotting/react-first.github.io.git/static/js/runtime-main.bc0bcd54.js"
+  }
+]);
